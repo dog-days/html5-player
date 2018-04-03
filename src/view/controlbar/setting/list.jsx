f4a4c90ce8acb613ce986614bf3cf41dd4934de4
xianshannan <xianshannan@qq.com>
2018-04-03 10:48:12 +0800

    fix controlbar not hide when selected speed or subtitle

--- src/view/controlbar/setting/list.jsx
+++ src/view/controlbar/setting/list.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 //内部依赖包
 import localization from '../../../i18n/default';
+import { namespace as videoNamespace } from '../../../model/video';
+import { CONTROLBAR_TIMEOUT } from '../../../utils/const';
 
 export default class List extends React.Component {
   static contextTypes = {
     localization: PropTypes.object,
+    playerDOM: PropTypes.object,
   };
   displayName = 'List';
   dispatch = this.props.dispatch;
@@ -15,6 +18,16 @@ export default class List extends React.Component {
       const { onSelect } = this.props;
       onSelect && onSelect(value, e);
       callback && callback();
+      if (!this.context.playerDOM.paused) {
+        //播放才处理，选择的时候因为鼠标位置已经离开了controlbar
+        //所有选择完成需要，触发隐藏controlbar
+        this.dispatch({
+          type: `${videoNamespace}/controlbar`,
+          payload: false,
+          delayTime: CONTROLBAR_TIMEOUT,
+          onControlbarEnter: false,
+        });
+      }
     };
   };
   getLocale() {
